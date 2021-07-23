91f1c168db88908e6a9014606ebe29e441c0db08
Ankit Gupta <80517800+AnkitGupta649@users.noreply.github.com>
2021-07-24 04:21:34 +0530

    Assignment Done

---
+++ module4 solution/AG.js
@@ -0,0 +1,23 @@
+var gupta=new Array();
+gupta[0]="Ankit";
+gupta[1]="Anurag";
+gupta[2]="krati";
+gupta[3]="perina";
+gupta[4]="manoj";
+gupta[5]="sarukh";
+gupta[6]="gutthi";
+gupta[7]="kapil";
+gupta[8]="jitendra";
+gupta[9]="akhshay";
+gupta[10]="Jeetu";
+for(var i=0;i<gupta.length;i++)
+{
+	if(gupta[i].charAt(0)==='J'||gupta[i].charAt(0)==='j')
+	{
+		console.log("Goodbye " + gupta[i]);
+	}
+	else
+	{
+		console.log("Hello "+gupta[i]);
+	}	
+}
